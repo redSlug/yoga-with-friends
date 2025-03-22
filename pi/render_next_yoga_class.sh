74abd7074e6190e5c40ea86428ceb5a280b87e67
Brad Dettmer <11279144+redSlug@users.noreply.github.com>
2025-03-22 16:37:43 -0400

    fix -eq: unary operator expected in bash script

--- pi/render_next_yoga_class.sh
+++ pi/render_next_yoga_class.sh
@@ -46,13 +46,13 @@ function main {
         SHOULD_RENDER=${should_render}
 
         ## if SHOULD_RENDER and NOT IS_RENDERING
-        if $SHOULD_RENDER && [ $IS_RENDERING -eq 1 ]; then
+        if [ $SHOULD_RENDER -eq 0 ] && [ $IS_RENDERING -eq 1 ]; then
           sudo rpi-rgb-led-matrix/examples-api-use/demo -D 1 yoga.ppm --led-no-hardware-pulse --led-rows=16 --led-cols=32 -m 0 --led-daemon --led-brightness=10
           IS_RENDERING=0
         fi
 
         ## if NOT SHOULD_RENDER and IS_RENDERING
-        if [ $SHOULD_RENDER -eq 1 ] && $IS_RENDERING; then
+        if [ $SHOULD_RENDER -eq 1 ] && [ $IS_RENDERING -eq 0]; then
           sudo pkill demo
           IS_RENDERING=1
         fi
